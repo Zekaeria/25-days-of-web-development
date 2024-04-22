b060910239b6c0cb3fe874956aab00c7c47a540b
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-04-22 09:21:35 -0700

    Add first draft
    clean up code

---
+++ day-05-profile-page/README.md
@@ -0,0 +1,7 @@
+# Day 5: ...
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/zekaeria/pen/YzMdyPM](https://codepen.io/zekaeria/pen/YzMdyPM).
+
+Recreation of Olha Uzhykova's website
+
+Image Source: Pawel Czerwinski on Unsplash
---
+++ day-05-profile-page/src/script.js
@@ -0,0 +1,14 @@
+//inspiration: https://olhauzhykova.com/
+//https://www.awwwards.com/inspiration/animation-olha-uzhykova-personal-website-1
+//todo: animate text reveal, add img, add parallax effect, import fontawesome
+//https://unsplash.com/photos/green-water-with-water-droplets-009elgvBM_A
+
+/* resources:
+flexbox: https://dev.to/dawnind/3-ways-to-display-two-divs-side-by-side-3d8b
+https://stackoverflow.com/questions/7706177/how-to-make-absolute-positioned-elements-move-with-the-rest-of-the-page
+https://fonts.google.com/specimen/Hina+Mincho?preview.text=Zekaeria%20Coding%20and%20Design&query=sat
+fontshare
+https://blog.hubspot.com/website/css-fade-in
+//https://www.tutorialrepublic.com/codelab.php?topic=faq&file=css-overlay-one-div-over-another-div
+//easings-css: https://www.npmjs.com/package/easings-css
+*/
