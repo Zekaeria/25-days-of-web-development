23daa55d8eb7a82588148a446265d7a704999839
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-04-24 08:05:28 -0700

    Add files via upload

---
+++ day-07-mood-board/README.md
@@ -0,0 +1,4 @@
+# Day 7: Mood Board (HSL Color + Grid Practice)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/zekaeria/pen/XWQOKOB](https://codepen.io/zekaeria/pen/XWQOKOB).
+
---
+++ day-07-mood-board/dist/script.js
@@ -0,0 +1,3 @@
+//edu resources: @summercodes, https://www.smashingmagazine.com/2021/07/hsl-colors-css/, https://css-tricks.com/snippets/css/complete-guide-grid/
+//images: "white fox sitting on snow during daytime" - Jonatan Pie
+//"clouds" - eberhardgross
