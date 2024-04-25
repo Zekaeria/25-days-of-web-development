ac505be6820a5e419c6656176f90c18ec5865248
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-04-24 22:29:32 -0700

    Set up functionality

---
+++ day-08-to-do-list-vue/README.md
@@ -0,0 +1,4 @@
+# Day 8: To Do List 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/zekaeria/pen/gOyqoRg](https://codepen.io/zekaeria/pen/gOyqoRg).
+
---
+++ day-08-to-do-list-vue/src/script.js
@@ -0,0 +1,22 @@
+new Vue({
+  el:'#app',
+  data: {
+    title: 'To Do List ',
+    todos: [
+      { text: 'Practice Vue', id:Date.now() },
+      { text: 'Get some sleep', id:Date.now()+1 }
+    ]
+  },
+  /* METHODS */
+  methods: {
+    /* Adds new task to 'todo' list array, v-for = access to parent scope properties */
+    addToDo(event) {
+      var text = event.target.value
+      this.todos.push({ text, id: Date.now() })
+      event.target.value = ''
+    },
+    removeToDo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  } 
+})
