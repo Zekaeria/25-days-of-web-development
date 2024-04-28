6d8491cfef633db81162aa909e0be041622e8b58
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-04-28 11:32:38 -0700

    Add files

--- day-11-just-keep-swimming-gsap-animation/README.md
+++ day-11-just-keep-swimming-gsap-animation/README.md
@@ -1,1 +1,4 @@
-...
+# Day 11:  'Just Keep Swimming' GSAP Animation
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/zekaeria/pen/abxxvpW](https://codepen.io/zekaeria/pen/abxxvpW).
+
---
+++ day-11-just-keep-swimming-gsap-animation/src/script.js
@@ -0,0 +1,8 @@
+//'Clownfish' by Rachel Hisko on Unsplash
+/* timeline sequence */
+var timeline = gsap.timeline({repeat: -1, repeatDelay: 1});
+
+timeline.to("img", {duration: 2, x: 400}); /* move forward to 400 pos */
+timeline.to("img", {scaleX: -1, x: 400}); /* flip horizontally */
+timeline.to("img", {duration: 2, x: 0}); /* move forward to start pos */
+timeline.to("img", {scaleX: 1, x: 0}); /* flip horizontally */
