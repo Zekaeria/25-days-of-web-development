311fdbd0ea02a7620ba07449224acc22e98cfdea
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-05-04 09:08:57 -0700

    Add GSAP animation

--- day-16-alien-title-sequence/dist/script.js
+++ day-16-alien-title-sequence/dist/script.js
@@ -1,1 +1,14 @@
-//
+/* timeline sequence */
+var timeline = gsap.timeline();
+
+timeline.from(".I", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".A1", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".N1", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".L1", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".E1", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".A2", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".N2", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".E2", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".L2", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".N3", {duration: 4, opacity: 0}, "+=1");
+timeline.from(".lastAE", {duration: 4, opacity: 0}, "+=1");
