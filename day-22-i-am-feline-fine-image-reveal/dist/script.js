45325e4da4f84b0394b2d17558c6c6ca92958b2f
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-05-10 19:09:54 -0700

    Fix repo files

--- day-22-i-am-feline-fine-image-reveal/dist/script.js
+++ day-22-i-am-feline-fine-image-reveal/dist/script.js
@@ -1,3 +1,12 @@
-//https://www.pixilart.com/draw
-//white #e7defc
-//https://colorhunt.co/palette/ffe6e6ffabe1a685e26155a6
+//Pixel art by me :)
+//Thank you to Laaouatni Anas for the help. https://stackoverflow.com/questions/70670068/mouse-movement-image-layer-effect
+//Thank you to Royal Smith for the static
+
+let circle = document.getElementById('circle');
+
+// on mouse move move the circle
+document.addEventListener('mousemove', (e) => {
+  // makes image move relative to the mouse
+  circle.style.left = e.pageX - 125 + 'px'; // 125 = half height of circle --> cursor is in the middle
+  circle.style.top = e.pageY - 125 + 'px';
+});
