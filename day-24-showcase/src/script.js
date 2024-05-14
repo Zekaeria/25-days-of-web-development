95b863190af3dc1d1e05066874d5b0fa7700960c
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2024-05-14 16:21:04 -0700

    Add image carousel

--- day-24-showcase/src/script.js
+++ day-24-showcase/src/script.js
@@ -1,3 +1,5 @@
 //https://codepen.io/gtrman97/pen/eYBQMwE
 //https://css-tricks.com/css-link-hover-effects/
 //features: top left profile pic + desc, title on right, scrollable projects on bottom
+//carousel: https://medium.com/@mintpw/how-to-create-infinite-horizontal-scroll-and-hover-pause-with-pure-css-b052caa683bd
+//https://forum.freecodecamp.org/t/how-to-get-an-image-of-your-codepen-project-for-your-profolio/468345/4
